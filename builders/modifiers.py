7ea00454fbed5ce2d4d102217e96f6d436f2fdbd
pupssman <pupssman@yandex-team.ru>
2014-01-17 18:46:42 +0400

    Add LambdaModifier to __all__

--- builders/modifiers.py
+++ builders/modifiers.py
@@ -2,7 +2,7 @@ import construct
 from builders.logger import logger
 
 
-__all__ = ['Modifier', 'InstanceModifier', 'ValuesMixin', 'ClazzModifier', 'ConstructModifier', 'Given', 'NumberOf', 'HavingIn', 'OneOf', 'Enabled']
+__all__ = ['Modifier', 'InstanceModifier', 'ValuesMixin', 'ClazzModifier', 'ConstructModifier', 'Given', 'NumberOf', 'HavingIn', 'OneOf', 'Enabled', 'LambdaModifier']
 
 
 class Modifier:
